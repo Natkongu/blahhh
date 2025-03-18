4cc8f45c604bb6dc8a7f3353841fad98c391e354
Natkongu <120584632+Natkongu@users.noreply.github.com>
2025-03-18 21:21:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var aside = document.querySelector("aside");
-var asideTop = aside.offsetTop;
-
-window.addEventListener("scroll", function () {
-    if (window.scrollY >= asideTop) {
-        aside.style.position = "fixed";
-        aside.style.top = "0px";
+/**
+ Set the scroll events for the HTML5 Scroll Load plugin and keeping the aside always in view.
+ */
+var asideTop = $('aside').offset().top - parseFloat($('aside').css('marginTop').replace(/auto/, 0));
+$(window).scroll(function () {
+    var asideY = $(this).scrollTop();
+    if (asideY >= asideTop) {
+        $('aside').css({
+            position: 'fixed',
+            top: '0px'
+        })
     } else {
-        aside.style.position = "sticky";
+        $('aside').removeAttr('style')
     }
 });
