005d85a02f10ab19e7b9021a2e30b6d5b02c5317
Natkongu <120584632+Natkongu@users.noreply.github.com>
2025-03-18 23:56:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var aside = iframeDocument.find('aside');
     var asideTop = aside.offset().top - parseFloat(aside.css('marginTop').replace(/auto/, 0));
     var stickPoint = 200; // Point where the aside becomes sticky
-    var unstickPoint = 800; // Point where the aside becomes unstuck
+    var unstickPoint = 20000; // Point where the aside becomes unstuck
 
     iframeDocument.scroll(function() {
         var asideY = $(this).scrollTop();
